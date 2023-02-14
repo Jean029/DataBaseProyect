ccd2264968bd39c55bbe290c803e40028ed3eb06
Jean C <jean.serrano7@upr.edu>
2023-02-14 07:44:26 -0400

    Modifique el modelo de los productos en el backen y el front end

--- backend_model/productsModel.py
+++ backend_model/productsModel.py
@@ -1,7 +1,16 @@
 # Done in array instead of dictionaries to portray the differences between dictionaries and arrays
 # Database tuples are normally received in an array
-productList = [['1', "Tello Drone", 'DJI', 'desc here', 'Yes', '480p', 'White', 'dji_tello.jpg', '15', 'active', '85.00', '85.00'],
-               ['2', 'Bebop 2', 'Parrot', 'desc', 'Yes', '1080p', 'Red', 'parrot_bebop_2.jpg', '3', 'active', '270.00', '290.00']]
+productList = [['1', 'Travel Scope', 'Celestron', 'description', 'Refractor', 'Altazimutal', '70', '400', 'TravelScope.png', '5', 'Active', '86', '86'],
+
+            ['2', 'Astromaster', 'Celestron', 'description', 'Reflector', 'Ecuatorial', '130', '650', 'AstroMaster.png', '3', 'Active', '300', '300'],
+
+            ['3', 'Advanced VX', 'Celestron', 'description', 'Catadioptrico', 'Ecuatorial', '235', '2359', 'AdvancedVX.jpg', '1', 'Active', '3,699', '3,699'],
+
+            ['4', 'Skyscanner 10012', 'Orion', 'description', 'Reflector', 'Altazimutal', '100', '400', 'Skyscanner.jpg', '2', 'Active', '130', '130'],
+
+            ['5', 'Maksutov-Cassegrain Starmax', 'Orion', 'description', 'Catadioptrico', 'Altazimutal', '90', '1250', 'Starmax.jpg', '1', 'Active', '180', '180'],
+
+            ['6', 'Astroview  9024', 'Orion', 'description', 'Refractor', 'Ecuatorial', '90', '910', 'AstroView.jpg', '4', 'Active', '250', '250']]
 
 
 def getProductsModel():
--- frontend_model/shopModel.py
+++ frontend_model/shopModel.py
@@ -1,20 +1,9 @@
-
+from backend_model.productsModel import productList
 # This is our simulation of the database
 # We have two products here.
 # The students must create their own productList when working on their eCommerce site
 # Product images are loaded into static/images/product-images/
 # Done in array instead of dictionaries to portray the differences
-productList = [['1', 'Travel Scope', 'Celestron', 'description', 'Refractor', 'Altazimutal', '70', '400', 'TravelScope.png', '5', 'Active', '86', '86'],
-
-            ['2', 'Astromaster', 'Celestron', 'description', 'Reflector', 'Ecuatorial', '130', '650', 'AstroMaster.png', '3', 'Active', '300', '300'],
-
-            ['3', 'Advanced VX', 'Celestron', 'description', 'Catadioptrico', 'Ecuatorial', '235', '2359', 'AdvancedVX.jpg', '1', 'Active', '3,699', '3,699'],
-
-            ['4', 'Skyscanner 10012', 'Orion', 'description', 'Reflector', 'Altazimutal', '100', '400', 'Skyscanner.jpg', '2', 'Active', '130', '130'],
-
-            ['5', 'Maksutov-Cassegrain Starmax', 'Orion', 'description', 'Catadioptrico', 'Altazimutal', '90', '1250', 'Starmax.jpg', '1', 'Active', '180', '180'],
-
-            ['6', 'Astroview  9024', 'Orion', 'description', 'Refractor', 'Ecuatorial', '90', '910', 'AstroView.jpg', '4', 'Active', '250', '250']]
 
 
 def getProductsModel():
