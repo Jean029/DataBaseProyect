5e0a201aa1e606b212aec3f4623038b0ebaeb730
Jean C <jean.serrano7@upr.edu>
2023-02-16 21:23:29 -0400

    Modificaciones de errores

--- frontend_model/loginModel.py
+++ frontend_model/loginModel.py
@@ -3,9 +3,9 @@ from flask import session
 # This is a very basic dictionary with information for logging in
 # Simulating our database
 thisDict = {
-    "email": ['jean@upr.edu', 'user@gmail.com'],
-    "password": ['pass1234', '1234'],
-    "user": ['Jean', 'User']
+    "email": ['jean@upr.edu', 'user@gmail.com', 'nebula@gmail.com'],
+    "password": ['pass1234', '1234', 'pass1234'],
+    "user": ['Jean', 'User', 'Nebula']
 }
 
 
